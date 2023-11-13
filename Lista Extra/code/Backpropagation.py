c280c81ab3dfd8236843c31277508ba946780aab
Eduardo Coelho <eceduardocoelho@gmail.com>
2023-11-13 16:07:12 -0300

    feat: extra list

--- Lista Extra/code/Backpropagation.py
+++ Lista Extra/code/Backpropagation.py
@@ -1,68 +1,74 @@
 import numpy as np
 
-# Funções de ativação
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
-def sigmoid_derivative(x):
-    return x * (1 - x)
+def predict(X, W1, W2):
+    Z1 = np.dot(X, W1)
+    A1 = sigmoid(Z1)
+    Z2 = np.dot(A1, W2)
+    A2 = sigmoid(Z2)
+    return A2
 
-# Classe da rede neural
-class NeuralNetwork:
-    def __init__(self, input_size):
-        self.input_size = input_size
-        self.output_size = 1
-        self.hidden_size = 4  # Número de neurônios na camada oculta
+def binary_cross_entropy(y_pred, y_true):
+    epsilon = 1e-15
+    y_pred = np.clip(y_pred, epsilon, 1 - epsilon)
+    return - (y_true * np.log(y_pred) + (1 - y_true) * np.log(1 - y_pred)).mean()
 
-        # Inicialização aleatória dos pesos
-        self.weights_input_hidden = np.random.rand(self.input_size, self.hidden_size)
-        self.weights_hidden_output = np.random.rand(self.hidden_size, self.output_size)
+def train_neural_network(X_train, y_train, learning_rate, iterations, hidden_size):
+    N, input_size = X_train.shape
+    W1 = np.random.normal(scale=0.5, size=(input_size, hidden_size))
+    W2 = np.random.normal(scale=0.5, size=(hidden_size, 1))
 
-    def forward(self, inputs):
-        # Propagação direta
-        self.hidden_input = np.dot(inputs, self.weights_input_hidden)
-        self.hidden_output = sigmoid(self.hidden_input)
-        self.output = sigmoid(np.dot(self.hidden_output, self.weights_hidden_output))
+    for itr in range(iterations):
+        # Feedforward propagation
+        A1 = sigmoid(np.dot(X_train, W1))
+        A2 = sigmoid(np.dot(A1, W2))
 
-        return self.output
+        # Backpropagation
+        E2 = A2 - y_train.reshape(-1, 1)
+        dW2 = np.dot(A1.T, E2) / N
+        E1 = np.dot(E2, W2.T) * A1 * (1 - A1)
+        dW1 = np.dot(X_train.T, E1) / N
 
-    def backward(self, inputs, target, learning_rate):
-        # Cálculo do erro
-        error = target - self.output
+        # Update weights
+        W2 = W2 - learning_rate * dW2
+        W1 = W1 - learning_rate * dW1
 
-        # Cálculo do delta da camada de saída
-        delta_output = error * sigmoid_derivative(self.output)
+    return W1, W2
 
-        # Cálculo do erro da camada oculta
-        error_hidden = delta_output.dot(self.weights_hidden_output.T)
+def main():
+    # Obter entradas do usuário
+    input_size = int(input("Número de entradas: "))
+    operation_type = input("Tipo de operação (AND, OR, XOR): ").upper()
 
-        # Cálculo do delta da camada oculta
-        delta_hidden = error_hidden * sigmoid_derivative(self.hidden_output)
+    # ...
 
-        # Atualização dos pesos
-        self.weights_hidden_output += np.outer(self.hidden_output, delta_output) * learning_rate
-        self.weights_input_hidden += np.outer(inputs, delta_hidden) * learning_rate
+    # Criar conjunto de treinamento
+    X_train = np.random.randint(2, size=(1000, input_size))
+    
+    if operation_type == "AND":
+        y_train = np.logical_and.reduce(X_train, axis=1)
+    elif operation_type == "OR":
+        y_train = np.logical_or.reduce(X_train, axis=1)
+    elif operation_type == "XOR":
+        y_train = np.logical_xor.reduce(X_train, axis=1)
 
-    def train(self, inputs, targets, learning_rate, epochs):
-        for _ in range(epochs):
-            for i in range(len(inputs)):
-                input_data = inputs[i]
-                target = targets[i]
-                self.forward(input_data)
-                self.backward(input_data, target, learning_rate)
+    # Adicionar coluna extra para bias
+    X_train = np.column_stack((X_train, np.ones(X_train.shape[0])))
 
-# Função de treinamento da porta XOR
-def train_xor(n):
-    input_data = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
-    targets = np.array([[0], [1], [1], [0]])
+    # Treinar a rede neural
+    learning_rate = 0.2
+    iterations = 20000
+    hidden_size = 8  # Aumente o tamanho da camada oculta
+    W1, W2 = train_neural_network(X_train, y_train, learning_rate, iterations, hidden_size)
 
-    nn = NeuralNetwork(n)
-    nn.train(input_data, targets, learning_rate=0.1, epochs=10000)
+    # Testar a rede neural
+    inputs = np.array([int(input(f"Insira a entrada {i + 1} (0 ou 1): ")) for i in range(input_size)])
+    input_with_bias = np.concatenate([inputs, [1]])  # Adicionar bias
+    result = round(predict(input_with_bias, W1, W2)[0])
 
-    for i in range(4):
-        output = nn.forward(input_data[i])
-        print(f'Input: {input_data[i]}, Predicted: {output[0]}, Actual: {targets[i][0]}')
+    print(f"Resultado da operação {operation_type} para as entradas {inputs}: {result}")
 
 if __name__ == "__main__":
-    n = int(input("Número de entradas: "))
-    train_xor(n)
+    main()
